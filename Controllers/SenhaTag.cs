f66fe9fefa519469956b5fb1ed67c402fd72f713
Rafael633 <rafael.souza2@alunos.sc.senac.br>
2022-06-28 06:21:39 -0300

    Correção da Branch

--- Controllers/SenhaTag.cs
+++ Controllers/SenhaTag.cs
@@ -9,42 +9,12 @@ namespace Controllers
     {
         public static SenhaTag IncluirSenhaTag(int SenhaId, int TagId)
         {
-            return new SenhaTag(SenhaId, TagId);
-        }
-
-        public static SenhaTag RemoverSenhaTag(int Id)
-        {
-            SenhaTag senhaTag = GetSenhaTag(Id);
-            SenhaTag.RemoverSenhaTag(senhaTag);
-            return senhaTag;
-        }
-
-        public static SenhaTag GetSenhaTag(int Id)
-        {
-            SenhaTag senhaTag = (
-                from SenhaTag in SenhaTag.GetSenhaTags()
-                    where SenhaTag.Id == Id
-                    select SenhaTag
-            ).First();
-
-            if(senhaTag == null)
+            if (TagId != null) 
             {
-                throw new Exception("SenhaTag não encontrada");
+                throw new Exception("É obrigatorio informar uma Tag");
             }
-
-            return senhaTag;
-        }
-
-        public static IEnumerable<SenhaTag> VisualizarSenhaTag()
-        {
-            return SenhaTag.GetSenhaTags();
-        }
-
-        public static SenhaTag GetById(int Id)
-        {
-            SenhaTag senhaTag = SenhaTag.GetById(Id);
-
-            return senhaTag;
+            
+            return new SenhaTag(SenhaId, TagId);
         }
     }
 }
