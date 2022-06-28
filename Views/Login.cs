fdd0e26e22b4540ee5fa81be332d298c34a0db8f
Rafael633 <rafael.souza2@alunos.sc.senac.br>
2022-06-28 06:59:40 -0300

    Correção das Branchs

--- Views/Login.cs
+++ Views/Login.cs
@@ -21,6 +21,7 @@ public class Login : Form //Tela de Login
 
     Button btnConfirm;
     Button btnCancel;
+    Button btnCadastrar;
 
     public Login()
     {
@@ -28,11 +29,11 @@ public class Login : Form //Tela de Login
 
         this.lblUser = new Label();
         this.lblUser.Text = "Usuário";
-        this.lblUser.Location = new Point(120, 20);
+        this.lblUser.Location = new Point(10, 20);
 
         this.lblPass = new Label();
         this.lblPass.Text = "Senha";
-        this.lblPass.Location = new Point(120, 80);
+        this.lblPass.Location = new Point(10, 83);
 
         this.txtUser = new TextBox();
         this.txtUser.Location = new Point(10, 50);
@@ -45,22 +46,29 @@ public class Login : Form //Tela de Login
 
         this.btnConfirm = new Button();
         this.btnConfirm.Text = "Confirmar";
-        this.btnConfirm.Location = new Point(100, 180);
+        this.btnConfirm.Location = new Point(106, 180);
         this.btnConfirm.Size = new Size(80, 30);
         this.btnConfirm.Click += new EventHandler(this.btnConfirmClick); 
 
         this.btnCancel = new Button();
         this.btnCancel.Text = "Cancelar";
-        this.btnCancel.Location = new Point(100, 220);
+        this.btnCancel.Location = new Point(106, 220);
         this.btnCancel.Size = new Size(80, 30);
-        this.btnCancel.Click += new EventHandler(this.btnCancelClick);  
+        this.btnCancel.Click += new EventHandler(this.btnCancelClick); 
+
+        this.btnCadastrar = new Button();
+        this.btnCadastrar.Text = "Cadastrar";
+        this.btnCadastrar.Location = new Point(106, 260);
+        this.btnCadastrar.Size = new Size(80, 30);
+        this.btnCadastrar.Click += new EventHandler(this.btnCadastrarClick);   
 
         this.Controls.Add(this.lblUser);
         this.Controls.Add(this.lblPass);        
         this.Controls.Add(this.txtUser);
         this.Controls.Add(this.txtPass);        
         this.Controls.Add(this.btnCancel);
-        this.Controls.Add(this.btnConfirm); 
+        this.Controls.Add(this.btnConfirm);
+        this.Controls.Add(this.btnCadastrar);
 
         this.components = new System.ComponentModel.Container();
         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -70,13 +78,25 @@ public class Login : Form //Tela de Login
 
     private void btnConfirmClick(object sender, EventArgs e)
     {
-        Inicio form = new Inicio();
-        form.Size = new Size(320, 245);
-        form.Show();
+        try
+        {
+            Usuario.Auth(this.txtUser.Text, this.txtPass.Text);
+            (new Inicio()).Show();
+        }
+        catch (Exception err)
+        {
+            MessageBox.Show(err.Message);
+        }
     }
 
     private void btnCancelClick(object sender, EventArgs e)
     {
         this.Close();
+    }
+
+    private void btnCadastrarClick(object sender, EventArgs e)
+    {
+        UsuarioInserir form = new UsuarioInserir();
+        form.Show();
     }
 }    
